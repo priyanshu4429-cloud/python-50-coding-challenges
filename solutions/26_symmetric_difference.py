d9a5c514d569162daab804db0db61b6e63f8ec52
Priyanshu Kumar <priyanshu4429@gmail.com>
2026-09-20 00:37:54 +0530

    Add solution 26: Symmetric Difference

---
+++ solutions/26_symmetric_difference.py
@@ -0,0 +1,9 @@
+﻿# HackerRank - Symmetric Difference
+# Topic: Sets | Difficulty: Easy
+
+n = int(input())
+a = set(map(int, input().split()))
+m = int(input())
+b = set(map(int, input().split()))
+result = sorted(a.symmetric_difference(b))
+print('\n'.join(map(str, result)))
