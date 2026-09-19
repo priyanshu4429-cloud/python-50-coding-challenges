e40139dca662658e33230bce2a05fd28226f274a
Priyanshu Kumar <priyanshu4429@gmail.com>
2026-09-20 00:37:53 +0530

    Add solution 24: Alphabet Rangoli

---
+++ solutions/24_alphabet_rangoli.py
@@ -0,0 +1,15 @@
+﻿# HackerRank - Alphabet Rangoli
+# Topic: Strings | Difficulty: Easy
+
+import string
+
+def rangoli(n):
+    alpha = string.ascii_lowercase
+    lines = []
+    for i in range(n):
+        s = '-'.join(alpha[i:n])
+        lines.append((s[::-1] + s[1:]).center(4*n-3, '-'))
+    print('\n'.join(lines[::-1] + lines[1:]))
+
+n = int(input())
+rangoli(n)
