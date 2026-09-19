02fea3e3f43ad651610edd7de0707cd02e72cffb
Priyanshu Kumar <priyanshu4429@gmail.com>
2026-09-20 00:37:59 +0530

    Add solution 40: Integers Come In All Sizes

---
+++ solutions/40_integers_come_in_all_sizes.py
@@ -0,0 +1,8 @@
+﻿# HackerRank - Integers Come In All Sizes
+# Topic: Built-ins | Difficulty: Easy
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(pow(a, b) + pow(c, d))
