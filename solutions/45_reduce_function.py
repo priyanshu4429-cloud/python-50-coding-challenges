b944fa7015b970901e7c7ea31d98a2ed05610f85
Priyanshu Kumar <priyanshu4429@gmail.com>
2026-09-20 00:38:00 +0530

    Add solution 45: Reduce Function

---
+++ solutions/45_reduce_function.py
@@ -0,0 +1,12 @@
+﻿# HackerRank - Reduce Function
+# Topic: Functional | Difficulty: Easy
+
+from functools import reduce
+
+def lcm(a, b):
+    from math import gcd
+    return a * b // gcd(a, b)
+
+n = int(input())
+nums = list(map(int, input().split()))
+print(reduce(lcm, nums))
