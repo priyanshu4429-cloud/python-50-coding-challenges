d20ac3d00977c093d6c24eb1497005c23c2f8a96
Priyanshu Kumar <priyanshu4429@gmail.com>
2026-09-20 00:37:49 +0530

    Add solution 10: Nested Lists

---
+++ solutions/10_nested_lists.py
@@ -0,0 +1,14 @@
+﻿# HackerRank - Nested Lists
+# Topic: Lists | Difficulty: Easy
+
+students = []
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    students.append([name, score])
+
+scores = sorted(set([s[1] for s in students]))
+second_lowest = scores[1]
+result = sorted([s[0] for s in students if s[1] == second_lowest])
+for name in result:
+    print(name)
